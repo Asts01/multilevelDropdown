53f6e156f54140849045859554da8c7face232a0
Asts01 <aasthajain22092003@gmail.com>
2024-09-03 22:48:01 +0530

    final-commit-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import HeaderComponent from './components/HeaderComponent'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <>
+      {/* <h1>MultiLevel-Navigation && React-Routers</h1> */}
       <HeaderComponent/>
+      <Home/>
       <Outlet/>
     </>
   )
