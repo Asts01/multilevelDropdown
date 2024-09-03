11f03740179944c254b75fc5053388e680517fa1
Asts01 <aasthajain22092003@gmail.com>
2024-09-03 22:57:00 +0530

    final-commit-4

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,8 +7,9 @@ export default function Home() {
             <h2 className='bg-pink-200 m-4 flex flex-col  text-green-800 fixed right-0'>
                     Welcome To Dice
             </h2>
-            <div className='m-4 w-auto flex justify-center'>
-            <img src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="Cant fetch data" className='m-4'/>
+            <div className='p-10'></div>
+            <div className='m-4 w-auto flex justify-center mt-3'>
+            <img src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="Cant fetch data" className='mt-5'/>
             </div>
         </div>
     );
